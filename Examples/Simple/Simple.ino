60fd80bff6e690b378eb07f0fd65d7f8a5dfacdd
Mike Schwager <mike@schwager.com>
2015-03-07 21:41:06 -0600

    Cleanup. Actually made this file simple! :-)

--- Examples/Simple/Simple.ino
+++ Examples/Simple/Simple.ino
@@ -1,10 +1,13 @@
 // EnableInterrupt Simple example sketch
-// See the Wiki at http://code.google.com/p/arduino-pinchangeint/wiki for more information.
+// See https://github.com/GreyGnome/EnableInterrupt and the README.md for more information.
 
 // for vim editing: :set et ts=2 sts=2 sw=2 et
 
 // This example demonstrates the use of the EnableInterrupt library on a single pin of your choice.
 // This has only been tested on an Arduino Duemilanove and Mega ADK.
+// It is designed to work with the Arduino Duemilanove/Uno, Arduino Mega2560/ADK, the Arduino
+// Leonardo, and the Arduino Due. Please 
+
 // To use:
 
 // 1. You must be using a fairly recent version of the Arduino IDE software on your PC/Mac,
@@ -26,18 +29,15 @@
 // variable. The sketch can then query the value at its leisure. This makes loop timing non-critical.
 // Open Tools->Serial Monitor in the IDE to see the results of your interrupts.
 
-// 5. See PinChangeIntExample328.ino (in the PinChangeInt distribution) for a more elaborate example.
+// 5. Peruse the Examples directory for more elaborate examples.
 
-// 6. Create your own sketch using the PinChangeInt library!
+// 6. Create your own sketch using the EnableInterrupt library!
 
 #include <EnableInterrupt.h>
-//#include <PinChangeInt.h>
 
 // Modify this at your leisure.
-// SS = PB0, A8 = PK0, 15 = PJ0, 14 = PJ1
-//#define ARDUINOPIN A10
+// On ATmega2560: SS = PB0, A8 = PK0, 15 = PJ0, 14 = PJ1
 #define ARDUINOPIN 8
-//#define ARDUINOPIN 14
 
 // Do not use any Serial.print() in interrupt subroutines. Serial.print() uses interrupts,
 // and by default interrupts are off in interrupt subroutines. Interrupt routines should also
@@ -45,81 +45,33 @@
 volatile uint16_t interruptCount=0; // The count will go back to 0 after hitting 65535.
 
 void interruptFunction() {
-  interruptSaysHello();
   interruptCount++;
 }
 // Notice that values that get modified inside an interrupt, that I wish to access
 // outside the interrupt, are marked "volatile". It tells the compiler not to optimize
 // the variable.
 
-/*
-volatile uint16_t interruptCount1=0; // The count will go back to 0 after hitting 65535.
-void interruptFunction2() {
-  interruptCount1++;
-}
-*/
-
-/*
-//extern volatile uint8_t current;
-extern volatile uint8_t risingPinsPORTC;
-extern volatile uint8_t fallingPinsPORTC;
-extern volatile uint8_t functionCalled;
-//extern volatile uint16_t interruptsCalled; // DEBUG
-extern volatile uint8_t changedPins;
-extern volatile uint8_t risingPins;
-extern volatile uint8_t fallingPins;
-*/
-
-
 // Attach the interrupt in setup()
 void setup() {
   //uint8_t pind, pink;
   Serial.begin(115200);
   Serial.println("---------------------------------------");
-  //PORTD=pind;
-  //PORTK=pink;
-  //pinMode(PINSIGNAL, OUTPUT);
   pinMode(ARDUINOPIN, INPUT_PULLUP);  // Configure the pin as an input, and turn on the pullup resistor.
                                       // See http://arduino.cc/en/Tutorial/DigitalPins
-  //PORTC=0x01;
+// To enable an external interrupt on the Arduino Uno, assuming ARDUINOPIN is 2 or 3:
+// enableInterrupt(ARDUINOPIN, interruptFunction, CHANGE);
+// To enable a pin change interrupt on the Arduino Uno, assuming ARDUINOPIN is 2 or 3:
+// enableInterrupt(ARDUINOPIN | PINCHANGEINTERRUPT, interruptFunction, CHANGE);
+// ...if you don't use the PINCHANGEINTERRUPT scheme, the pin will be configured with an External interrupt.
   enableInterrupt(ARDUINOPIN, interruptFunction, CHANGE);
-  //attachPinChangeInterrupt(ARDUINOPIN, interruptFunction, CHANGE);
-  //current=PINC;
-  interruptSaysHello(); // to turn on Signal (LED on 328-based Arduino, 21 on Mega)
 }
 
 // In the loop, we just check to see where the interrupt count is at. The value gets updated by the
 // interrupt routine.
 void loop() {
-  uint8_t led_on, led_off;         // DEBUG
-
-  led_on=0b00100000; led_off=~led_on;
-
-  //*led_port|=led_mask; // LED high
-  //PORTB|=led_on;
-  //PORTC=0x01;
   Serial.println("---------------------------------------");
   delay(1000);                          // Every second,
-  //PORTC=0x00; // software interrupt, port A0
-  //PORTB|=led_on;
-  //PORTB&=led_off;
-  //PORTB|=led_on;
-  //PORTB&=led_off;
-  //PORTB|=led_on;
-
-  //PORTB&=led_off;
-  //PORTB|=led_on;
   Serial.print("Pin was interrupted: ");
   Serial.print(interruptCount, DEC);      // print the interrupt count.
   Serial.println(" times so far.");
-
-  //Serial.print(" isr's: ");
-  //Serial.print(interruptsCalled, DEC);
-  //Serial.print(" current: 0x");
-  //Serial.print(current, HEX);
-  //Serial.print(" changedPins: 0x");
-  //Serial.print(changedPins, HEX);
-  //Serial.print(" function: ");
-  //Serial.println(functionCalled, DEC);
-  //functionCalled=0;
 }
