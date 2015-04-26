b3484929391385d57f02332069a5487ac82c3496
Mike Schwager <mike@schwager.com>
2015-04-26 17:50:55 -0500

    Initial commit

--- examples/HiSpeedTest/HiSpeedTest.ino
+++ examples/HiSpeedTest/HiSpeedTest.ino
@@ -3,23 +3,52 @@
 //
 // https://github.com/GreyGnome/EnableInterrupt/wiki/Usage#Summary
 
+//#define OLDLIBRARY
+
 #define NEEDFORSPEED
 
-#define NOTIFY A5
+#define THEINTERRUPTPIN8
+// Define the above, and undefine this for External Interrupt measurements.
+//#define THEINTERRUPTPIN2 
 
-#define PIN8 8
-#define PIN8_ON PORTB |= (1 << PB0)
-#define PIN8_OFF PORTB &= ~(1 << PB0)
+#if defined THEINTERRUPTPIN8
+#define PININTERRUPT 8
+#define PININTERRUPT_ON PORTB |= (1 << PB0)
+#define PININTERRUPT_OFF PORTB &= ~(1 << PB0)
+#define THEINTERRUPTVARIABLE myvariable_pin8
 #ifdef NEEDFORSPEED
-#define INTERRUPT_FLAG_PIN8 myvariable_pin8 // NOTICE: NO semicolon!!!
+#define INTERRUPT_FLAG_PIN8 myvariable_pin8 // NOTICE: NO semicolon!!! This needs to track the PININTERRUPT pin
 #else
 volatile uint8_t myvariable_pin8=0;
 #endif
+#endif
 
+#if defined THEINTERRUPTPIN2
+#define PININTERRUPT 2
+#define PININTERRUPT_ON PORTD |= (1 << PD2)
+#define PININTERRUPT_OFF PORTD &= ~(1 << PD2)
+#define THEINTERRUPTVARIABLE myvariable_pin2
+#ifdef NEEDFORSPEED
+#define INTERRUPT_FLAG_PIN2 myvariable_pin2 // NOTICE: NO semicolon!!! This needs to track the PININTERRUPT pin
+#else
+volatile uint8_t myvariable_pin2=0;
+#endif
+#endif
+
+
+#ifdef OLDLIBRARY
+#include <PinChangeInt.h>
+#define EI_printPSTR(x) SerialPrint_P(PSTR(x))
+void SerialPrint_P(const char *str) {
+	  for (uint8_t c; (c = pgm_read_byte(str)); str++) Serial.write(c);
+}
+#else
 #include <EnableInterrupt.h>
+#endif
+#include <avr/cpufunc.h>
 
 void incrementMyVariable() { // The compiler is smart: only uses memory if needed (ie, NEEDFORSPEED not defined)
-  myvariable_pin8++;
+  THEINTERRUPTVARIABLE++;
 }
 
 // Attach the interrupt in setup()
@@ -27,14 +56,18 @@ void setup() {
   //uint8_t pind, pink;
   Serial.begin(115200);
   EI_printPSTR("---------------------------------------\r\n");
-  pinMode(PIN8, INPUT_PULLUP);  // Configure the pin as an input, and turn on the pullup resistor.
-                                      // See http://arduino.cc/en/Tutorial/DigitalPins
-  pinMode(NOTIFY, OUTPUT);  // Configure the pin as an output
-#ifdef NEEDFORSPEED
-  enableInterruptFast(PIN8, CHANGE);
+  pinMode(PININTERRUPT, OUTPUT);  // Configure the pin as an output
+  PININTERRUPT_OFF;
+#ifdef OLDLIBRARY
+  attachPinChangeInterrupt(PININTERRUPT, incrementMyVariable, CHANGE);
 #else
-  enableInterrupt(PIN8, incrementMyVariable, CHANGE);
+#ifdef NEEDFORSPEED
+  enableInterruptFast(PININTERRUPT, CHANGE);
+#else
+  enableInterrupt(PININTERRUPT, incrementMyVariable, CHANGE);
 #endif
+#endif
+  EI_printPSTR("Interrupt enabled, let's go!");
 }
 
 // In the loop, we just check to see where the interrupt count is at. The value gets updated by the
@@ -43,19 +76,21 @@ void loop() {
 
   EI_printPSTR("---------------------------------------\r\n");
   delay(1000);                          // Every second,
-  PIN8_ON; 	// software interrupt
-  PORTB |= (1 << PB0); // MIKE TEST DEBUG
-  PORTC |= (1 << PC5);  // == A5. We will enter the interrupt.
-  //PORTC=0x01;
-  //PORTC=0x00; // software interrupt, port A0
-  delay(2000); // Every second,
-  PORTC |= (1 << PC5);  // == A5. We have left the interrupt.
-  PORTC &= ~(1 << PC5); // == A5. 
-  if (myvariable_pin8) {
+  PININTERRUPT_ON; 	// software interrupt
+  _NOP();		// See "8-bit AVR Microcontroller with 4/8/16/32K Bytes In-System Programmable
+  _NOP();		// Flash" document, Rev. 8271C-AVR-08/10, p. 71.
+  _NOP();		// The interrupt signal must be held until PCIFR is changed, in order for the
+#if defined THEINTERRUPTPIN8
+  _NOP();               // CPU to execute the ISR. This requires 4 nop assembler instructions.
+#else
+                        // (three nop's for an External Interrupt)
+#endif
+  PININTERRUPT_OFF;     // ...This is after the interrupt.
+  if (THEINTERRUPTVARIABLE > 0) {
     EI_printPSTR("Pin was interrupted: ");
-    Serial.print(myvariable_pin8, DEC);      // print the interrupt count.
+    Serial.print(THEINTERRUPTVARIABLE, DEC);      // print the interrupt count.
     EI_printPSTR(" times so far.\r\n");
-    myvariable_pin8=0;
+    THEINTERRUPTVARIABLE=0;
   }
   else {
     EI_printPSTR("No interrupts.\r\n");
