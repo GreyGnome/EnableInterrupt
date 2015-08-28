b444aedde167f6fd36d0b7af1969ca5959c63e41
Mike Schwager <mike@schwager.com>
2015-08-27 22:05:08 -0500

    Small tweaks

--- examples/HiSpeedTest/HiSpeedTest.ino
+++ examples/HiSpeedTest/HiSpeedTest.ino
@@ -7,23 +7,25 @@
 
 #define NEEDFORSPEED
 
-#define THEINTERRUPTPIN8
+#define USEINTERRUPTPIN8 // PORTB
 // Define the above, and undefine this for External Interrupt measurements.
-//#define THEINTERRUPTPIN2 
+//#define USEINTERRUPTPIN2 
 
-#if defined THEINTERRUPTPIN8
+#if defined USEINTERRUPTPIN8
 #define PININTERRUPT 8
 #define PININTERRUPT_ON PORTB |= (1 << PB0)
 #define PININTERRUPT_OFF PORTB &= ~(1 << PB0)
 #define THEINTERRUPTVARIABLE myvariable_pin8
+//#define EI_NOTPORTD // will not produce __vector_5 code
 #ifdef NEEDFORSPEED
 #define INTERRUPT_FLAG_PIN8 myvariable_pin8 // NOTICE: NO semicolon!!! This needs to track the PININTERRUPT pin
 #else
+#define EI_NOTPORTB
 volatile uint8_t myvariable_pin8=0;
 #endif
 #endif
 
-#if defined THEINTERRUPTPIN2
+#if defined USEINTERRUPTPIN2 // PORTD
 #define PININTERRUPT 2
 #define PININTERRUPT_ON PORTD |= (1 << PD2)
 #define PININTERRUPT_OFF PORTD &= ~(1 << PD2)
@@ -47,7 +49,7 @@ void SerialPrint_P(const char *str) {
 #endif
 #include <avr/cpufunc.h>
 
-void incrementMyVariable() { // The compiler is smart: only uses memory if needed (ie, NEEDFORSPEED not defined)
+void incrementMyVariable() { // This isn't used or compiled if NEEDFORSPEED is defined.
   THEINTERRUPTVARIABLE++;
 }
 
@@ -67,7 +69,10 @@ void setup() {
   enableInterrupt(PININTERRUPT, incrementMyVariable, CHANGE);
 #endif
 #endif
-  EI_printPSTR("Interrupt enabled, let's go!");
+  EI_printPSTR("Interrupt enabled, let's go!\r\n");
+  EI_printPSTR("Make sure nothing is connected to pin ");
+  Serial.println(PININTERRUPT, DEC);
+  EI_printPSTR("This sketch sends a software interrupt to that pin.\r\n");
 }
 
 // In the loop, we just check to see where the interrupt count is at. The value gets updated by the
@@ -80,7 +85,7 @@ void loop() {
   _NOP();		// See "8-bit AVR Microcontroller with 4/8/16/32K Bytes In-System Programmable
   _NOP();		// Flash" document, Rev. 8271C-AVR-08/10, p. 71.
   _NOP();		// The interrupt signal must be held until PCIFR is changed, in order for the
-#if defined THEINTERRUPTPIN8
+#if defined USEINTERRUPTPIN8
   _NOP();               // CPU to execute the ISR. This requires 4 nop assembler instructions.
 #else
                         // (three nop's for an External Interrupt)
@@ -89,7 +94,7 @@ void loop() {
   if (THEINTERRUPTVARIABLE > 0) {
     EI_printPSTR("Pin was interrupted: ");
     Serial.print(THEINTERRUPTVARIABLE, DEC);      // print the interrupt count.
-    EI_printPSTR(" times so far.\r\n");
+    EI_printPSTR(" times this iteration.\r\n");
     THEINTERRUPTVARIABLE=0;
   }
   else {
