719b030ca13d9363a9f30e277d763bfee2b7d20a
Mike Schwager <mike@schwager.com>
2015-09-15 06:40:45 -0500

    Only one port on Leonardo, so I got rid of some 'if (portNumber == PB)' conditions.

--- utility/ei_PinChangeLeonardo.h
+++ utility/ei_PinChangeLeonardo.h
@@ -1,40 +1,33 @@
+// ALL pin change interrupts are on Port B on Leonardo.
 #ifdef EI_SECTION_RISING
 #ifndef EI_NOTPORTB
-if (portNumber==PB) { 
-  risingPinsPORTB |= portMask;
-}
+risingPinsPORTB |= portMask;
 #endif
 #endif // EI_SECTION_RISING
 
 #ifdef EI_SECTION_FALLING
 #ifndef EI_NOTPORTB
-if (portNumber==PB) { 
-  fallingPinsPORTB |= portMask;
-}
+fallingPinsPORTB |= portMask;
 #endif
 #endif // EI_SECTION_FALLING
 
 #if defined EI_SECTION_ASSIGNFUNCTIONSREGISTERS
 #ifndef EI_NOTPORTB
-if (portNumber==PB) {
 #ifndef NEEDFORSPEED
-  calculatedPointer=&portBFunctions.pinZero + portBitNumber;
-  *calculatedPointer = userFunction;
+calculatedPointer=&portBFunctions.pinZero + portBitNumber;
+*calculatedPointer = userFunction;
 #endif
-  portSnapshotB=*portInputRegister(portNumber);
-  pcmsk=&PCMSK0;
-  PCICR |= _BV(0);
-}
+portSnapshotB=*portInputRegister(portNumber);
+pcmsk=&PCMSK0;
+PCICR |= _BV(0);
 #endif
 #endif // EI_SECTION_ASSIGNFUNCTIONSREGISTERS
 
 #ifdef EI_SECTION_DISABLEPINCHANGE
 #ifndef EI_NOTPORTB
-if (portNumber == PB) {
-  PCMSK0 &= ~portMask;
-  if (PCMSK0 == 0) { PCICR &= ~_BV(0); };
-  risingPinsPORTB &= ~portMask;
-  fallingPinsPORTB &= ~portMask;
-}
+PCMSK0 &= ~portMask;
+if (PCMSK0 == 0) { PCICR &= ~_BV(0); };
+risingPinsPORTB &= ~portMask;
+fallingPinsPORTB &= ~portMask;
 #endif
 #endif // EI_SECTION_DISABLEPINCHANGE
