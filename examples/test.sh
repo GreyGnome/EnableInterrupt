d7ea6549c324ad6f3603da2ad0b09e1c99e38c06
Mike Schwager <mike@schwager.com>
2015-09-29 06:29:08 -0500

    Compile test script.

---
+++ examples/test.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+examples="
+AllPins2560 
+AllPins328 
+ATtinyBlink 
+HiSpeed 
+HiSpeedAllPins2560 
+HiSpeedAllPins328 
+InterruptedPin2560 
+InterruptedPin328
+Mighty1284p
+OOSimple
+Simple
+"
+
+for example in $examples; do
+	echo $example
+	sleep 2
+	( cd $example; rm -rf build-*; make)
+	echo $example done
+	sleep 2
+done
